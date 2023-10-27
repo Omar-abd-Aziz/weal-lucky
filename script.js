7ffcdcf1c04fb9bc076b1ffcf2b58c9edce24032
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2023-10-27 15:44:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ const selectPrize = () => {
     width: 600,
     padding: '3em',
     color: '#716add',
-    background: '#fff url(/images/trees.png)',
+    background: '#fff url(./images/trees.png)',
     backdrop: `
       rgba(0,0,123,0.4)
       url("${randomImage}")
